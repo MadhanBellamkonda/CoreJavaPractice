50e4b5101fe87b082acc77e16f1283d9b025f601
madhan <b.madhan90301@gmail.com>
2019-01-28 19:44:12 +0530

    first commit

---
+++ StaticExp.java
@@ -0,0 +1,24 @@
+package com.test.patterns;
+
+public class StaticExp {
+
+		int id;
+		String name;
+		static int fee;
+		StaticExp (int r,String f){
+			id=r;
+			name=f;
+		}	
+			void display(){
+				
+			}
+			
+		
+		
+		
+	public static void main(String[] args) {
+		
+	
+}
+
+}
